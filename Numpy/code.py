6d686dad67d161c366d8d2c734369b11d95b9dbe
sunilkumarcheruku <sunilkumarcheruku@users.noreply.github.com>
2020-04-23 01:05:58 +0530

    Code file created.

---
+++ Numpy/code.py
@@ -0,0 +1,92 @@
+# --------------
+# Importing header files
+import numpy as np
+
+# Path of the file has been stored in variable called 'path'
+
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+
+#Code starts here
+data = np.genfromtxt(path, delimiter =",", skip_header = 1)
+
+census = np.concatenate((data, np.array(new_record)))
+
+print(census)
+
+
+# --------------
+#Code starts here
+age = census[:, 0]
+print(age)
+
+max_age = np.max(age)
+min_age = np.min(age)
+age_mean = age.mean()
+age_std = age.std()
+
+print(max_age)
+print(min_age)
+print(age_mean)
+print(age_std)
+
+
+
+
+# --------------
+#Code starts here
+
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+
+len_0 = len(race_0)
+len_1 = len(race_1)
+len_2 = len(race_2)
+len_3 = len(race_3)
+len_4 = len(race_4)
+
+print(len_0)
+print(len_1)
+print(len_2)
+print(len_3)
+print(len_4)
+
+
+len_array = np.array([len_0, len_1, len_2, len_3, len_4])
+
+minority_race = np.argmin(len_array)
+
+print(minority_race)
+
+
+# --------------
+#Code starts here
+senior_citizens = census[census[:,0] > 60]
+
+working_hours_sum = sum(senior_citizens[:,6])
+print(working_hours_sum)
+
+senior_citizens_len = len(senior_citizens)
+print(senior_citizens_len)
+
+avg_working_hours = working_hours_sum/senior_citizens_len
+
+print(avg_working_hours)
+
+
+# --------------
+#Code starts here
+high = census[census[:,1] > 10]
+low  = census[census[:,1] <= 10]
+
+avg_pay_high = high[:,7].mean()
+avg_pay_low = low[:,7].mean()
+
+print(avg_pay_high)
+
+print(avg_pay_low)
+
+
